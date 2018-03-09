ff67ec985c078441f5cf402c4a04c6dfe228af0c
WTS <wbtsilva@gmail.com>
2018-03-09 11:59:04 +0000

    fixed disposableincome script

--- spending-gen/main.py
+++ spending-gen/main.py
@@ -5,6 +5,12 @@
 # disposableincome related
 # it's shite
 # Groceries, rent, transport, bills, shopping, eating out, general, income
+# can't believe i forgot a pen
+# random number between 0 and 10
+# 0-4 -> no change
+# 4-6 -> shopping
+# 6-9 -> eating_out
+# 9-10 -> general
 
 import csv
 import random
@@ -49,23 +55,20 @@ def expensereport():
 def disposableincome():
     with open('disposable.csv', 'w', newline='') as csv_file:
         disposableincome = csv.writer(csv_file, delimiter=',')
-        # disposableincome.writerow(['Test', 'Test1', 'Test2'])
         monthlength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        rent = 1300                                                             # taken
-        transport = 92                                                          # taken
+        incomeleft = 0
+        monthsalary = 1700                                                      # added
+        rent = 1300                                                             # expended
+        transport = 92                                                          # expensed
         grocery_base = 130                                                      # n/a
         bills_base = 120                                                        # n/a
         for month in monthlength:
-            incomeleft = 2163                                                   # n/a
-            grocery = math.trunc(grocery_base * random.uniform(0.7, 1.5))       # taken
-            bills = math.trunc(bills_base * random.uniform(0.7, 1.5))           # taken
-            shopping = random.randrange(50)
-            eating_out = random.randrange(80)
-            general = random.randrange(30)
-            # tys_eating_number = random.randint(5)
-            # tys_eating = ()
-            # tys_general = ()
-            # tys_shopping = ()
+            incomeleft += monthsalary                                           # n/a
+            grocery = math.trunc(grocery_base * random.uniform(0.7, 1.5))       # expensed
+            bills = math.trunc(bills_base * random.uniform(0.7, 1.5))           # expensed
+            shopping = random.randrange(300)                                    # expensed
+            eating_out = random.randrange(200)                                  # expensed
+            general = random.randrange(70)                                      # expensed
             for i in range(1, month+1):
                 if i is 26:
                     incomeleft -= rent
@@ -77,6 +80,19 @@ def disposableincome():
                     incomeleft = incomeleft - transport
                 if i%6 == 0:
                     incomeleft = incomeleft - grocery // 4
+                tys_mod = random.randint(0, 10)
+                if tys_mod >= 0 & tys_mod < 4:
+                    # no change
+                    incomeleft = incomeleft
+                elif tys_mod >= 4 & tys_mod < 6:
+                    # shopping charge
+                    incomeleft -= shopping
+                elif tys_mod >= 6 & tys_mod < 9:
+                    # eating out charge
+                    incomeleft -= eating_out
+                elif tys_mod >= 9 & tys_mod <= 10:
+                    # general charge
+                    incomeleft -= general
                 disposableincome.writerow([incomeleft])
 
 
